7d2eb1774b048708276a2d88c204621d034d4880
Albert Kaimenyi <albertkaimenyi254@gmail.com>
2025-05-20 11:57:56 +0300

    homepage callback

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: sized_box_for_whitespace
-
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:medical_app/pages/appointments_page.dart';
